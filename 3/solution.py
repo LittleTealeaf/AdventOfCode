24b82875d238ca6128b22d702516e240cbb1a4cf
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-06 17:52:44 -0500

    Completed part 3 of advent calendar

--- 3/solution.py
+++ 3/solution.py
@@ -19,3 +19,33 @@ with open("input.txt") as file:
         else:
             total += ord(letter) - ord('a') + 1
     print(total)
+
+with open("input.txt") as file:
+    total = 0
+    
+    buffer = []
+    for index, line in enumerate(file.readlines()):
+        value = '\0'
+        if index % 3 == 0:
+            buffer = []
+            for char in line:
+                if char not in buffer and char != '\n':
+                    buffer.append(char)
+        elif index % 3 == 1:
+            bf = []
+            for char in line:
+                if char in buffer:
+                    bf.append(char)
+            buffer = bf
+        elif index % 3 == 2:
+            for char in line:
+                if char in buffer:
+                    value = char
+            if ord(value) < ord('a'):
+                total += ord(value) - ord('A') + 27
+            else:
+                total += ord(value) - ord('a') + 1
+    print(total)
+
+        
+
