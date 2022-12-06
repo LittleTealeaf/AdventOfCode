3424c0887b35a6547863147c6b03e38bfb6c1da0
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-06 18:08:43 -0500

    Completed day 4

--- 4/solution.py
+++ 4/solution.py
@@ -10,5 +10,14 @@ with open("input.txt") as file:
         if (a_a <= b_a and a_b >= b_b) or (b_a <= a_a and b_b >= a_b):
             count += 1
     print(count)
+
+with open("input.txt") as file:
+    count = 0
+    for line in file.readlines():
+        ranges = [[int(i) for i in elf.split("-")] for elf in line.split(",")]
+
+        if (ranges[0][1] >= ranges[1][0] and ranges[0][0] <= ranges[1][1]) or (ranges[1][1] >= ranges[0][0] and ranges[0][1] >= ranges[1][0]):
+            count += 1
+    print(count)
         
 
