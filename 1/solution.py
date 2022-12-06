ae1f950212fa9cecd5a44156cb62f9431a36a3f3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-06 11:41:29 -0500

    Completed day 1 solution

--- 1/solution.py
+++ 1/solution.py
@@ -7,5 +7,20 @@ with open("input.txt") as file:
                 max_calories = calories
             calories = 0
         else:
-           calories += int(line)
+            calories += int(line)
     print(f'Max Calories: {max_calories}')
+
+
+with open("input.txt") as file:
+    leaderboard = [0,0,0]
+    calories = 0
+    for line in file.readlines():
+        if line == '\n':
+            if min(leaderboard) < calories:
+                leaderboard.append(calories)
+                leaderboard.sort()
+                leaderboard.pop(0)
+            calories = 0
+        else:
+            calories += int(line)
+    print(f"Calories: {sum(leaderboard)}")
