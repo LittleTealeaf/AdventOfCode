ea085d9b439a656d0305eb9e6ca9442c04705432
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-20 14:11:01 -0500

    2022 day 1 rust full solution

--- 2022/1/solution.rs
+++ 2022/1/solution.rs
@@ -1,10 +1,14 @@
 use std::fs::File;
-use std::io::{self,BufRead};
+use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    if let Ok(lines) = read_lines("./input.txt") {
+    part_1();
+    part_2();
+}
 
+fn part_1() {
+    if let Ok(lines) = read_lines("./input.txt") {
         let mut max = 0;
         let mut calories = 0;
 
@@ -26,13 +30,41 @@ fn main() {
             max = calories;
         }
 
-        println!("Max Calories is {}", max);
+        println!("Part 1: Max Calories is {}", max);
+    }
+}
 
+fn part_2() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut leaderboard: Vec<i32> = Vec::new();
+        let mut calories = 0;
+
+        for line in lines {
+            if let Ok(line) = line {
+                if line == String::from("") {
+                    leaderboard.push(calories);
+                    calories = 0;
+                } else {
+                    let val: i32 = line.parse().expect("Invalid number");
+                    calories += val;
+                }
+            }
+        }
+
+        leaderboard.push(calories);
+
+        leaderboard.sort();
+        leaderboard.reverse();
+        let sum: i32 = leaderboard[..3].iter().sum();
+
+        println!("Part 2: Sum of top 3 is {}", sum);
     }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
