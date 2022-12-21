6ee5d0641acece0909e314a40f4673efea8b2be4
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-21 18:55:23 -0500

    Completed 2022 Day 9 Part 2

--- 2022/9/solution.py
+++ 2022/9/solution.py
@@ -1,30 +1,50 @@
-with open("input.txt") as file:
-    positions = [(0,0)]
-
-    hx = 0
-    hy = 0
-    tx = 0
-    ty = 0
-
+def decode_steps(lines: list[str]):
     direction = {
         "R": (1, 0),
         "L": (-1, 0),
         "U": (0, 1),
         "D": (0, -1)
     }
+    value: list[tuple[tuple[int,int],int]] = []
+    for line in lines:
+        [a,b] = line.split(" ")
+        value.append((direction[a],int(b)))
+    return value
 
-    for line in file.readlines():
-        [a, b] = line.split(" ")
-        (dx,dy) = direction[a]
-        steps = int(b)
-        for i in range(steps):
+def simulate(moves: list[tuple[tuple[int,int],int]], size: int):
+    positions: list[tuple[int,int]] = [(0, 0)]
+    hx = 0
+    hy = 0
+    tx = 0
+    ty = 0
+
+    tail_moves = []
+    for (dx,dy),step in moves:
+        for i in range(step):
             hx += dx
             hy += dy
             if abs(hx - tx) > 1 or abs(hy - ty) > 1:
+                tdx = 0
+                tdy = 0
                 if abs(hx - tx) > 0:
-                    tx += abs(hx - tx) / (hx - tx)
+                    dx1 = abs(hx - tx) // (hx - tx)
+                    tx += dx1
+                    tdx = dx1
                 if abs(hy - ty) > 0:
-                    ty += abs(hy - ty) / (hy - ty)
+                    dy1 = abs(hy - ty) // (hy - ty)
+                    ty += dy1
+                    tdy = dy1
+                tail_moves.append(((tdx,tdy),1))
                 if (tx,ty) not in positions:
                     positions.append((tx,ty))
-    print(f"Part 1: The tail visited {len(positions)} unique positions")
+    if size > 1:
+        return simulate(tail_moves,size-1)
+    else:
+        return len(positions)
+
+with open("input.txt") as file:
+    steps = decode_steps(file.readlines())
+    part_1 = simulate(steps,1)
+    print(f"Part 1: {part_1} unique positions")
+    part_2 = simulate(steps,9)
+    print(f"Part 2: {part_2} unique positions")
