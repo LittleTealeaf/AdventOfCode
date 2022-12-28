84a1055b6958d3504fe56d8c2de152781b68a026
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-27 22:07:00 -0500

    Completed Part 2 of 2022 Day 4

--- 2022/4/solution.rs
+++ 2022/4/solution.rs
@@ -3,6 +3,7 @@ use std::io::{self, BufRead};
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -37,4 +38,37 @@ fn part_1() {
     }
 
     println!("Part 1: Count is {}", count);
+}
+
+fn part_2() {
+    let file = File::open("input.txt").expect("");
+    let lines = io::BufReader::new(file).lines();
+
+    let mut count = 0;
+
+    for line in lines {
+        if let Ok(line) = line {
+            let line_segments: Vec<Vec<u64>> = line
+                .split(",")
+                .map(|val: &str| -> Vec<u64> {
+                    val.split("-")
+                        .map(|n| -> u64 { n.parse().expect("Invalid") })
+                        .collect()
+                })
+                .collect();
+            let (first, second) = {
+                if line_segments[0][0] < line_segments[1][0] {
+                    (&line_segments[0], &line_segments[1])
+                } else {
+                    (&line_segments[1], &line_segments[0])
+                }
+            };
+
+            if first[1] >= second[0] {
+                count += 1;
+            }
+        }
+    }
+
+    println!("Part 2: Count is {}", count);
 }
