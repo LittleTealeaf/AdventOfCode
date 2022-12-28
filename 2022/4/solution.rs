af7395101dc77832dddb7ad8f46437b86d89986d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-27 21:58:02 -0500

    Rust in 2022 Part 4

---
+++ 2022/4/solution.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    part_1();
+}
+
+fn part_1() {
+    let file = File::open("input.txt").expect("");
+    let lines = io::BufReader::new(file).lines();
+    let mut count = 0;
+
+    for line in lines {
+        if let Ok(line) = line {
+            let line_segments: Vec<Vec<u64>> = line
+                .split(",")
+                .map(|val: &str| -> Vec<u64> {
+                    val.split("-")
+                        .map(|n| -> u64 { n.parse().expect("Invalid") })
+                        .collect()
+                })
+                .collect();
+            let (inner, outer) = {
+                if line_segments[0][1] - line_segments[0][0]
+                    < line_segments[1][1] - line_segments[1][0]
+                {
+                    (&line_segments[0], &line_segments[1])
+                } else {
+                    (&line_segments[1], &line_segments[0])
+                }
+            };
+
+            if outer[0] <= inner[0] && inner[1] <= outer[1] {
+                count += 1;
+            }
+        }
+    }
+
+    println!("Part 1: Count is {}", count);
+}
