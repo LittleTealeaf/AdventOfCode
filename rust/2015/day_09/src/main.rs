c535aa70213008c4d00c421f08730090cecbc863
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-28 17:41:35 -0400

    2015 Day 9 Part 1

--- rust/2015/day_09/src/main.rs
+++ rust/2015/day_09/src/main.rs
@@ -1,16 +1,17 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    ops::Index,
 };
 
 #[derive(Debug, Clone)]
-struct Edge {
+struct Route {
     from: String,
     to: String,
     distance: i32,
 }
 
-impl From<String> for Edge {
+impl From<String> for Route {
     fn from(value: String) -> Self {
         let tokens = value.split(" ").collect::<Vec<_>>();
 
@@ -22,22 +23,122 @@ impl From<String> for Edge {
     }
 }
 
-fn get_nodes(edges: &Vec<Edge>) -> Vec<String> {
+#[derive(Debug)]
+struct Edge {
+    endpoints: [usize; 2],
+    distance: i32,
+}
+
+impl Edge {
+    fn has_node(&self, node: usize) -> bool {
+        self.endpoints[0] == node || self.endpoints[1] == node
+    }
+}
+
+fn index_of<T: PartialEq>(array: &Vec<T>, item: &T) -> Option<usize> {
+    for i in 0..(array.len()) {
+        if array[i].eq(&item) {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn convert_routes_to_edges(routes: Vec<Route>) -> (Vec<Edge>, usize) {
+    let mut edges = Vec::new();
     let mut nodes = Vec::new();
 
+    for Route { from, to, distance } in routes {
+        let endpoints = [
+            match index_of(&nodes, &from) {
+                Some(index) => index,
+                None => {
+                    nodes.push(from);
+                    nodes.len() - 1
+                }
+            },
+            match index_of(&nodes, &to) {
+                Some(index) => index,
+                None => {
+                    nodes.push(to);
+                    nodes.len() - 1
+                }
+            },
+        ];
+
+        edges.push(Edge {
+            endpoints,
+            distance,
+        });
+    }
+
+    (edges, nodes.len())
+}
+
+struct Snapshot {
+    current_node: usize,
+    distance_traveled: i32,
+    nodes_left: Vec<usize>,
+}
+
+fn part_1(routes: Vec<Route>) -> i32 {
+    let (mut edges, node_count) = convert_routes_to_edges(routes);
+    edges.sort_by_key(|edge| -> i32 { edge.distance });
+    let [start, _] = edges.last().unwrap().endpoints;
+
+    let mut distances: Vec<Vec<i32>> = Vec::new();
+
+    for _ in 0..node_count {
+        let mut vec = Vec::new();
+        for _ in 0..node_count {
+            vec.push(-1);
+        }
+        distances.push(vec);
+    }
+
     for edge in edges {
-        if !nodes.contains(&edge.from) {
-            nodes.push(edge.from.clone());
+        let [start, end] = edge.endpoints;
+        distances[start][end] = edge.distance;
+        distances[end][start] = edge.distance;
+    }
+
+    let mut snapshots = {
+        let nodes_left = (0..node_count)
+            .filter(|i| -> bool { i != &start })
+            .collect::<Vec<_>>();
+
+        vec![Snapshot {
+            current_node: start,
+            distance_traveled: 0,
+            nodes_left,
+        }]
+    };
+
+    let mut min_distance = -1;
+
+    while let Some(snapshot) = snapshots.pop() {
+        if snapshot.nodes_left.len() == 0 {
+            if min_distance == -1 || min_distance > snapshot.distance_traveled {
+                min_distance = snapshot.distance_traveled;
+            }
+            continue;
+        }
+        
+        let mut nodes_clone = snapshot.nodes_left.clone();
+
+        for next in snapshot.nodes_left {
+            let index = index_of(&nodes_clone, &next).unwrap();
+            nodes_clone.remove(index);
+            snapshots.push(Snapshot {
+                current_node: next,
+                distance_traveled: (snapshot.distance_traveled + distances[next][snapshot.current_node]),
+                nodes_left: nodes_clone.clone()
+            });
+            nodes_clone.push(next);
         }
     }
 
-    nodes
-}
-
-fn part_1(edges: Vec<Edge>) -> i32 {
-    let distance = 0;
-
-    distance
+    min_distance
 }
 
 fn main() {
@@ -47,9 +148,10 @@ fn main() {
             .lines()
             .into_iter()
             .map(Result::unwrap)
-            .map(Edge::from)
+            .map(Route::from)
             .collect::<Vec<_>>();
-        part_1(edges);
+        let result = part_1(edges);
+        println!("Part 1: {}", result);
     }
 }
 
@@ -68,7 +170,7 @@ mod tests {
         let edges = strings
             .into_iter()
             .map(String::from)
-            .map(Edge::from)
+            .map(Route::from)
             .collect::<Vec<_>>();
 
         let value = part_1(edges);
