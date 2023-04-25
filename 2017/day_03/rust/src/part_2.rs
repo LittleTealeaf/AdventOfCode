77a13f6c7f1617f3a98f16c0be2aff98bd310a1c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-25 15:35:39 -0400

    comment out loop, this is a TO BE COMPLETED project

--- 2017/day_03/rust/src/part_2.rs
+++ 2017/day_03/rust/src/part_2.rs
@@ -2,28 +2,30 @@ pub fn part_2(number: i64) -> i64 {
     let mut previous_layer = vec![1];
     let mut layer = 2;
 
-    loop {
-        previous_layer.reverse();
-        let mut layer_values: Vec<i64> = Vec::new();
+    // loop {
+    //     previous_layer.reverse();
+    //     let mut layer_values: Vec<i64> = Vec::new();
+    //
+    //     let mut prev_layer_buffer: Vec<i64> = Vec::new();
+    //     prev_layer_buffer.push(*previous_layer.last().unwrap());
+    //
+    //     let layer_size = calculate_layer_size(layer);
+    //     let mut dist = layer - 2;
+    //     let mut change = -1;
+    //
+    //     for i in 0..layer_size {
+    //         let mut value = layer_values.last().unwrap_or(&0).clone();
+    //
+    //         if dist == 0 {
+    //             change = 1;
+    //         } else if dist == layer - 1 {
+    //             change = -1;
+    //         }
+    //         dist += change;
+    //     }
+    // }
 
-        let mut prev_layer_buffer: Vec<i64> = Vec::new();
-        prev_layer_buffer.push(*previous_layer.last().unwrap());
-
-        let layer_size = calculate_layer_size(layer);
-        let mut dist = layer - 2;
-        let mut change = -1;
-
-        for i in 0..layer_size {
-            let mut value = layer_values.last().unwrap_or(&0).clone();
-
-            if dist == 0 {
-                change = 1;
-            } else if dist == layer - 1 {
-                change = -1;
-            }
-            dist += change;
-        }
-    }
+    0
 }
 
 fn calculate_layer_size(layer: i64) -> i64 {
