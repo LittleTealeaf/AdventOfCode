c74e24f739b97385fa9edc4ceade272131e06ece
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-15 13:23:14 -0500

    changes

---
+++ 2015/24/rust/src/main.rs
@@ -0,0 +1,83 @@
+fn main() {
+    let input = include_str!("../../input.txt")
+        .lines()
+        .map(|line| line.trim().parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+
+    println!("Part 1: {}", part_1(&input));
+}
+
+fn part_1(input: &Vec<u64>) -> u64 {
+    let input = input.clone();
+    let goal = input.iter().sum::<u64>() / 3;
+
+    #[derive(Clone, Debug)]
+    struct State {
+        g1_sum: u64,
+        g1_prod: Option<u64>,
+        g1_len: usize,
+        g2_sum: u64,
+        stack: Vec<u64>,
+        goal_sum: u64,
+    }
+
+    impl State {
+        fn recusive(self, mut min_length: usize) -> Option<Self> {
+            if self.g1_len > min_length {
+                None
+            } else if self.g1_sum < self.goal_sum {
+                let mut min: Option<State> = None;
+
+                for i in 0..self.stack.len() {
+                    let mut state = self.clone();
+                    let value = state.stack.swap_remove(i);
+                    state.g1_sum += value;
+                    state.g1_len += 1;
+
+                    if let Some(val) = state.g1_prod {
+                        state.g1_prod = val.checked_mul(value)
+                    }
+
+                    if let Some(result) = state.clone().recusive(min_length) {
+                        min_length = result.g1_len;
+                        if let Some(m) = &min {
+                            if m.g1_prod > result.g1_prod {
+                                min = Some(result)
+                            }
+                        } else {
+                            min = Some(result);
+                        }
+                    }
+                }
+                min
+            } else if self.g1_sum > self.goal_sum {
+                None
+            } else if self.g2_sum < self.goal_sum {
+                (0..self.stack.len()).find_map(|i| {
+                    let mut state = self.clone();
+                    let value = state.stack.swap_remove(i);
+                    state.g2_sum += value;
+                    state.recusive(min_length)
+                })
+            } else if self.g2_sum > self.goal_sum {
+                None
+            } else {
+                Some(self)
+            }
+        }
+    }
+
+    let state = State {
+        g1_sum: 0,
+        g1_len: 0,
+        g1_prod: Some(1),
+        g2_sum: 0,
+        goal_sum: goal,
+        stack: input,
+    };
+
+    let result = state.recusive(usize::MAX).unwrap();
+    println!("{:?}", result);
+
+    0
+}
