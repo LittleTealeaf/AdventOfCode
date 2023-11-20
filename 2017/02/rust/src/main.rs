8b96d79645146d3c4d3ec5134b1a3fdd21366d5d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-20 08:21:30 -0500

    2017 02 in rust

---
+++ 2017/02/rust/src/main.rs
@@ -0,0 +1,41 @@
+fn part_1(spreadsheet: &Vec<Vec<i32>>) -> u32 {
+    spreadsheet
+        .into_iter()
+        .map(|row| {
+            row.iter()
+                .max()
+                .unwrap()
+                .abs_diff(*row.iter().min().unwrap())
+        })
+        .sum()
+}
+
+fn part_2(spreadsheet: &Vec<Vec<i32>>) -> i32 {
+    spreadsheet
+        .into_iter()
+        .map(|row| {
+            for i in 0..row.len() {
+                for j in 0..row.len() {
+                    if i != j && row[i] % row[j] == 0 {
+                        return row[i] / row[j];
+                    }
+                }
+            }
+            return 0;
+        })
+        .sum()
+}
+
+fn main() {
+    let spreadsheet = include_str!("../../input.txt")
+        .lines()
+        .map(|line| {
+            line.split("\t")
+                .filter_map(|token| token.trim().parse::<i32>().ok())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    println!("Part 1: {}", part_1(&spreadsheet));
+    println!("Part 2: {}", part_2(&spreadsheet));
+}
