d13b637a36c949af96bac3c743127a681ee913e5
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-19 13:59:40 -0500

    refactor 22 09

--- 2022/09/rust/src/main.rs
+++ 2022/09/rust/src/main.rs
@@ -1,17 +1,9 @@
+use std::collections::HashSet;
+
 fn main() {
-    let input = include_str!("../../input.txt")
-        .lines()
-        .filter_map(|line| Instruction::try_from(line).ok())
-        .collect::<Vec<_>>();
-
-    println!("Part 1: {}", part_1(&input));
-    println!("Part 2: {}", part_2(&input));
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Instruction {
-    direction: Direction,
-    steps: i32,
+    let solution = Solution::new(include_str!("../../input.txt"));
+    println!("Part 1: {}", solution.part_1());
+    println!("Part 2: {}", solution.part_2());
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -22,237 +14,150 @@ enum Direction {
     Right,
 }
 
-impl TryFrom<&str> for Instruction {
-    type Error = ();
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let mut tokens = value.trim().split(" ");
-        let direction = match tokens.next().unwrap() {
-            "U" => Ok(Direction::Up),
-            "D" => Ok(Direction::Down),
-            "L" => Ok(Direction::Left),
-            "R" => Ok(Direction::Right),
-            _ => Err(()),
-        }?;
-        let steps = tokens.next().unwrap().parse::<i32>().unwrap();
-
-        Ok(Self { direction, steps })
-    }
+#[derive(Clone, Copy, Debug)]
+struct Instruction {
+    direction: Direction,
+    steps: i32,
 }
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Hash, Default)]
 struct Pos {
     x: i32,
     y: i32,
 }
 
-fn part_1(input: &Vec<Instruction>) -> usize {
-    let mut head = Pos { x: 0, y: 0 };
-    let mut tail = Pos { x: 0, y: 0 };
+struct Solution {
+    instructions: Vec<Instruction>,
+}
 
-    let mut positions = vec![Pos { x: 0, y: 0 }];
-
-    for instruction in input {
-        for _ in 0..instruction.steps {
-            match instruction.direction {
-                Direction::Up => {
-                    head.y += 1;
-                    if tail.y < head.y - 1 {
-                        tail.y = head.y - 1;
-                        tail.x = head.x;
-                    }
-                }
-                Direction::Down => {
-                    head.y -= 1;
-                    if tail.y > head.y + 1 {
-                        tail.y = head.y + 1;
-                        tail.x = head.x;
-                    }
-                }
-                Direction::Left => {
-                    head.x -= 1;
-                    if tail.x > head.x + 1 {
-                        tail.x = head.x + 1;
-                        tail.y = head.y;
-                    }
-                }
-                Direction::Right => {
-                    head.x += 1;
-                    if tail.x < head.x - 1 {
-                        tail.x = head.x - 1;
-                        tail.y = head.y;
-                    }
-                }
-            }
-
-            if !positions.contains(&tail) {
-                positions.push(tail)
-            }
+impl Solution {
+    fn new(input: &str) -> Self {
+        Self {
+            instructions: input
+                .lines()
+                .map(|line| {
+                    let mut tokens = line.trim().split(' ');
+                    let direction = match tokens.next().unwrap() {
+                        "U" => Direction::Up,
+                        "D" => Direction::Down,
+                        "L" => Direction::Left,
+                        "R" => Direction::Right,
+                        _ => panic!(),
+                    };
+                    let steps = tokens.next().unwrap().parse::<i32>().unwrap();
+                    Instruction { direction, steps }
+                })
+                .collect(),
         }
     }
 
-    positions.len()
+    fn part_1(&self) -> usize {
+        let mut head = Pos::default();
+        let mut tail = Pos::default();
+
+        let mut positions = HashSet::from([Pos::default()]);
+
+        for instruction in &self.instructions {
+            for _ in 0..instruction.steps {
+                match instruction.direction {
+                    Direction::Up => {
+                        head.y += 1;
+                    }
+                    Direction::Down => {
+                        head.y -= 1;
+                    }
+                    Direction::Left => {
+                        head.x -= 1;
+                    }
+                    Direction::Right => {
+                        head.x += 1;
+                    }
+                }
+
+                let x_off = head.x.abs_diff(tail.x) > 1;
+                let y_off = head.y.abs_diff(tail.y) > 1;
+                match (x_off, y_off) {
+                    (true, false) => {
+                        tail.x = (tail.x + head.x) / 2;
+                        tail.y = head.y;
+                    }
+                    (false, true) => {
+                        tail.y = (tail.y + head.y) / 2;
+                        tail.x = head.x;
+                    }
+                    (true, true) => {
+                        tail.x = (tail.x + head.x) / 2;
+                        tail.y = (tail.y + head.y) / 2;
+                    }
+                    _ => {}
+                }
+
+                positions.insert(tail);
+            }
+        }
+
+        positions.len()
+    }
+
+    fn part_2(&self) -> usize {
+        let mut nodes = [Pos { x: 0, y: 0 }; 10];
+
+        let mut positions = vec![Pos { x: 0, y: 0 }];
+
+        for instr in &self.instructions {
+            for _ in 0..instr.steps {
+                match instr.direction {
+                    Direction::Up => nodes[0].y += 1,
+                    Direction::Down => nodes[0].y -= 1,
+                    Direction::Left => nodes[0].x -= 1,
+                    Direction::Right => nodes[0].x += 1,
+                }
+
+                for i in 1..10 {
+                    let hor_off = nodes[i - 1].x.abs_diff(nodes[i].x) > 1;
+                    let ver_off = nodes[i - 1].y.abs_diff(nodes[i].y) > 1;
+
+                    match (hor_off, ver_off) {
+                        (true, false) => {
+                            nodes[i].x = (nodes[i].x + nodes[i - 1].x) / 2;
+                            nodes[i].y = nodes[i - 1].y;
+                        }
+                        (false, true) => {
+                            nodes[i].y = (nodes[i].y + nodes[i - 1].y) / 2;
+                            nodes[i].x = nodes[i - 1].x;
+                        }
+                        (true, true) => {
+                            nodes[i].x = (nodes[i].x + nodes[i - 1].x) / 2;
+                            nodes[i].y = (nodes[i].y + nodes[i - 1].y) / 2;
+                        }
+                        _ => {}
+                    }
+                }
+
+                if !positions.contains(&nodes[9]) {
+                    positions.push(nodes[9]);
+                }
+            }
+        }
+
+        positions.len()
+    }
 }
 
 #[test]
 fn test_part_1() {
-    let instructions = vec![
-        Instruction {
-            direction: Direction::Right,
-            steps: 4,
-        },
-        Instruction {
-            direction: Direction::Up,
-            steps: 4,
-        },
-        Instruction {
-            direction: Direction::Left,
-            steps: 3,
-        },
-        Instruction {
-            direction: Direction::Down,
-            steps: 1,
-        },
-        Instruction {
-            direction: Direction::Right,
-            steps: 4,
-        },
-        Instruction {
-            direction: Direction::Down,
-            steps: 1,
-        },
-        Instruction {
-            direction: Direction::Left,
-            steps: 5,
-        },
-        Instruction {
-            direction: Direction::Right,
-            steps: 2,
-        },
-    ];
-
-    assert_eq!(part_1(&instructions), 13);
-}
-
-fn part_2(input: &Vec<Instruction>) -> usize {
-    let mut nodes = [Pos { x: 0, y: 0 }; 10];
-
-    let mut positions = vec![Pos { x: 0, y: 0 }];
-
-    for instr in input {
-        for _ in 0..instr.steps {
-            match instr.direction {
-                Direction::Up => nodes[0].y += 1,
-                Direction::Down => nodes[0].y -= 1,
-                Direction::Left => nodes[0].x -= 1,
-                Direction::Right => nodes[0].x += 1,
-            }
-
-            for i in 1..10 {
-                let hor_off = nodes[i - 1].x.abs_diff(nodes[i].x) > 1;
-                let ver_off = nodes[i - 1].y.abs_diff(nodes[i].y) > 1;
-
-                match (hor_off, ver_off) {
-                    (true, false) => {
-                        nodes[i].x = (nodes[i].x + nodes[i - 1].x) / 2;
-                        nodes[i].y = nodes[i - 1].y;
-                    }
-                    (false, true) => {
-                        nodes[i].y = (nodes[i].y + nodes[i - 1].y) / 2;
-                        nodes[i].x = nodes[i - 1].x;
-                    }
-                    (true, true) => {
-                        nodes[i].x = (nodes[i].x + nodes[i - 1].x) / 2;
-                        nodes[i].y = (nodes[i].y + nodes[i - 1].y) / 2;
-                    }
-                    _ => {}
-                }
-            }
-
-            if !positions.contains(&nodes[9]) {
-                positions.push(nodes[9]);
-            }
-        }
-    }
-
-    positions.len()
+    let solution = Solution::new(include_str!("../../sample_1.txt"));
+    assert_eq!(solution.part_1(), 13);
 }
 
 #[test]
-fn test_1_as_2() {
-    let instructions = vec![
-        Instruction {
-            direction: Direction::Right,
-            steps: 4,
-        },
-        Instruction {
-            direction: Direction::Up,
-            steps: 4,
-        },
-        Instruction {
-            direction: Direction::Left,
-            steps: 3,
-        },
-        Instruction {
-            direction: Direction::Down,
-            steps: 1,
-        },
-        Instruction {
-            direction: Direction::Right,
-            steps: 4,
-        },
-        Instruction {
-            direction: Direction::Down,
-            steps: 1,
-        },
-        Instruction {
-            direction: Direction::Left,
-            steps: 5,
-        },
-        Instruction {
-            direction: Direction::Right,
-            steps: 2,
-        },
-    ];
-
-    assert_eq!(part_2(&instructions), 1);
+fn test_part_2_sample_1() {
+    let solution = Solution::new(include_str!("../../sample_1.txt"));
+    assert_eq!(solution.part_2(), 1);
 }
 
 #[test]
-fn test_part_2() {
-    let instructions = vec![
-        Instruction {
-            direction: Direction::Right,
-            steps: 5,
-        },
-        Instruction {
-            direction: Direction::Up,
-            steps: 8,
-        },
-        Instruction {
-            direction: Direction::Left,
-            steps: 8,
-        },
-        Instruction {
-            direction: Direction::Down,
-            steps: 3,
-        },
-        Instruction {
-            direction: Direction::Right,
-            steps: 17,
-        },
-        Instruction {
-            direction: Direction::Down,
-            steps: 10,
-        },
-        Instruction {
-            direction: Direction::Left,
-            steps: 25,
-        },
-        Instruction {
-            direction: Direction::Up,
-            steps: 20,
-        },
-    ];
-
-    assert_eq!(part_2(&instructions), 36);
+fn test_part_2_sample_2() {
+    let solution = Solution::new(include_str!("../../sample_2.txt"));
+    assert_eq!(solution.part_2(), 36);
 }
